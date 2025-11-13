7ccdbc0a63119833b6571ca5ba67b5e14954287c
Eimante.Bogdan <eimante.bogdan@gmail.com>
2025-11-13 09:25:27 +0200

    Created helper file with cleanup function

--- tests/user-controller.test.ts
+++ tests/user-controller.test.ts
@@ -1,40 +1,13 @@
 // tests/api.spec.ts
 import { test, expect } from '@playwright/test';
 import {StatusCodes} from "http-status-codes";
+import { cleanupUsers } from '../src/cleanup';
 let baseURL: string = 'http://localhost:3000/users';
 
 test.describe('User management API - all other tests', () => {
 
     test.beforeEach(async ({request}) => {
-        // get all users
-        const responseAllUsers = await request.get(`${baseURL}`);
-        expect(responseAllUsers.status()).toBe(StatusCodes.OK);
-
-        const responseUsers = await responseAllUsers.json()
-        const numberOfObjects = responseUsers.length;
-        console.log('Existing users before test: ' + numberOfObjects);
-
-        let userIDs: number[] = [];
-
-        // loop through all users and store their ID in an array
-        for (let i = 0; i < numberOfObjects; i++) {
-            let userID = responseUsers[i].id;
-            userIDs.push(userID);
-        }
-        console.log('userIDs', userIDs);
-
-        // delete all users
-        for (let i = 0; i < numberOfObjects; i++) {
-            let response = await request.delete(`${baseURL}/${userIDs[i]}`);
-            expect.soft(response.status()).toBe(200);
-        }
-
-        // verify cleanup
-        const finalCheck = await request.get(`${baseURL}`);
-        expect(finalCheck.status()).toBe(StatusCodes.OK);
-        const responseBody = await finalCheck.json()
-        expect(responseBody).toEqual([]);
-
+        await cleanupUsers(request);
     });
 
     // TESTS
